5cfa4a9845c97f07457da0064574a2f571e81b08
renan lima <renangenito@hotmail.com>
2021-12-22 12:45:29 -0300

    Testando a lógica e deixando responsivo para mobile

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const botaoCalcular = document.getElementById('botao-calcular')
-
-
-
+const imcResultado = document.getElementById('imc-resultado')
+const vermelho = document.getElementById('vermelho')
+const amarelo = document.getElementById('amarelo')
+const verde = document.getElementById('verde')
 
 
 function calculaImc(paciente){
     const imc = paciente.peso / (paciente.altura * paciente.altura)
+    testaImc(imc)
+
     return imc.toFixed(2);
  }
 
@@ -16,7 +19,59 @@ botaoCalcular.addEventListener('click', (event)=>{
         peso: document.getElementById('peso').value,
         altura: document.getElementById('altura').value
        }
-       
-    console.log(`${paciente.nome} o Seu IMC é ${calculaImc(paciente)} portanto está OK`)
+       imcResultado.textContent = `${paciente.nome} o Seu IMC é
+        ${calculaImc(paciente)}`
+        document.getElementById('formulario-imc').reset()
 })
 
+function sinalVermelho(){
+    apagaFarol()
+    limpaText()
+    vermelho.classList.add('vermelho')
+}
+function sinalAmarelo(){
+    apagaFarol()
+    limpaText()
+    amarelo.classList.add('amarelo')
+}
+function sinalVerde(){
+    apagaFarol()
+    limpaText()
+    verde.classList.add('verde')
+}
+
+function apagaFarol(){
+    vermelho.classList.remove('vermelho')
+    amarelo.classList.remove('amarelo')
+    verde.classList.remove('verde')
+}
+function limpaText(){
+    vermelho.textContent = ""
+    amarelo.textContent = ""
+    verde.textContent = ""
+}
+
+function testaImc(imc){
+   
+    if(imc < 17){
+        sinalVermelho()
+        vermelho.textContent = "Muito abaixo do peso!!!"
+    }
+    if(imc >= 17 && imc <= 18.49){
+        sinalAmarelo()
+        amarelo.textContent = "Abaixo do peso!!"
+    }    
+    if(imc >= 18.50 && imc <= 24.99){
+        sinalVerde()
+        verde.textContent = "Parabés você está com o seu peso ideal!"
+    }
+    if(imc >= 25 && imc <= 29.99){
+        sinalAmarelo()
+        amarelo.textContent = "Opa, está acima do peso!!"
+    }
+    if(imc > 30){
+        sinalVermelho()
+        vermelho.textContent = "Muito acima do peso!!!"
+    }
+    
+}
